a5060ddf6a018ed8bdad6d0ebfd7dad5ef372090
Parashoot <parezivale@gmail.com>
2019-03-05 10:40:18 -0600

    Working on file schooser

--- guitar-app/guitartutor.py
+++ guitar-app/guitartutor.py
@@ -1,15 +1,16 @@
 '''
 This is a prototype of our guitar tutoring app.
 
-The app is implemented in Kivy and python 3. 
+The app is implemented in Kivy and python 3.
 
 Current Contributions: Gerardo Mares II
 
 Gerardo Mares II Notes:
-	
+
 '''
 
 # Used to get directory to different 'screens'
+import os
 from os.path import dirname, join
 # Defalut kivy app object, we need this to make kivy work
 from kivy.app import App
@@ -19,34 +20,77 @@ from kivy.properties import NumericProperty, StringProperty, BooleanProperty, Li
 from kivy.uix.screenmanager import Screen
 # Use builder to run kivy code inside of python
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.uix.popup import Popup
 
 # This will be the class representing each screen
 # There is currently no logic in each screen
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
 class GuitarScreen(Screen):
 	fullscreen = BooleanProperty(False)
-	
+
 	# This function adds the widget to the window, we need this to display the pages
 	def add_widget(self, *args):
 		if 'content' in self.ids:
 			return self.ids.content.add_widget(*args)
 		return super(GuitarScreen, self).add_widget(*args)
+		
+	def dismiss_popup(self):
+		self._popup.dismiss()
+
+	def show_load(self):
+		content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+		self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+		self._popup.open()
+
+	def show_save(self):
+		content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+		self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
+		self._popup.open()
+
+	def load(self, path, filename):
+		with open(os.path.join(path, filename[0])) as stream:
+			self.text_input.text = stream.read()
+		self.dismiss_popup()
+
+	def save(self, path, filename):
+		with open(os.path.join(path, filename), 'w') as stream:
+			stream.write(self.text_input.text)
+		self.dismiss_popup()
 	
+
 class GuitarApp(App):
+	loadfile = ObjectProperty(None)
+	savefile = ObjectProperty(None)
+	text_input = ObjectProperty(None)
+	
 	# Use index to cycle through screens
 	index = NumericProperty(-1)
 	# Keep all screens in a list to access later
 	screen_names = ListProperty([])
 	# Title of current screen
 	current_title = StringProperty()
-	
+
 	def build(self):
 		# Title of window
 		self.title = 'Guitar Tutor'
-		# Initialize the list of screen that app has 
+		# Initialize the list of screen that app has
 		self.screens = {}
 		# Add screens to the list
 		self.available_screens = ["HomeScreen", "ChordLibrary",
-			"TabLibrary","AddTab","Challenge"]
+			"TabLibrary", "AddTab", "Challenge"]
 		# Remember names of screens, used for loading files
 		self.screen_names = self.available_screens
 		# Get current directory
@@ -56,12 +100,12 @@ class GuitarApp(App):
 			'{}.kv'.format(fn).lower()) for fn in self.available_screens]
 		# Initialize first screen (index 0)
 		self.go_screen(0)
-	
+
 	def go_screen(self, idx):
 		if(self.index != idx):
 			self.index = idx
 			self.root.ids.sm.switch_to(self.load_screen(idx), direction="left")
-		
+
 	# returns a screen
 	# might be modified to search for next screen... maybe
 	def load_screen(self, index):
@@ -69,7 +113,9 @@ class GuitarApp(App):
 			return self.screens[index]
 		screen = Builder.load_file(self.available_screens[index])
 		self.screens[index] = screen
-		return screen	
-		
+		return screen
+
+
+
 if __name__ == '__main__':
 	GuitarApp().run()
