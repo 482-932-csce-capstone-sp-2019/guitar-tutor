3b42b95f2b117d5c3b35d189ad85d77625c11144
Parashoot <parezivale@gmail.com>
2019-03-07 09:29:38 -0600

    Merging

--- guitar-app/guitartutor.py
+++ guitar-app/guitartutor.py
@@ -118,7 +118,7 @@ class GuitarApp(App):
 	# Used in chord library
 	# Displays the given chord on the guitar
 	def displayChord(self, chord):
-		cl()2
+		cl()
 		chords(chord)
 
 	def getRandomChord(self):
