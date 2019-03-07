99f80bb30a3449d6485d7b908a52bab54dcf290f
Parashoot <parezivale@gmail.com>
2019-03-07 10:11:20 -0600

    Saving the FIles

--- guitartutor.py
+++ guitartutor.py
@@ -34,7 +34,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
 from kivy.uix.popup import Popup
 
-import os, sys
+import sys
+import shutil
 a = os.path.abspath(os.path.join('.','Arduino'))
 sys.path.append(a)
 b = os.path.abspath(os.path.join('.', 'Parsing'))
@@ -64,12 +65,6 @@ class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
-
-class SaveDialog(FloatLayout):
-    save = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
 class GuitarScreen(Screen):
 	fullscreen = BooleanProperty(False)
 
@@ -87,21 +82,9 @@ class GuitarScreen(Screen):
 		self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
 		self._popup.open()
 
-	def show_save(self):
-		content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-		self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
-		self._popup.open()
-
 	def load(self, path, filename):
-		with open(os.path.join(path, filename[0])) as stream:
-			self.text_input.text = stream.read()
+		shutil.copy(os.path.join(path, filename[0]), os.path.abspath(os.path.join('.','data/tabs')))
 		self.dismiss_popup()
-
-	def save(self, path, filename):
-		with open(os.path.join(path, filename), 'w') as stream:
-			stream.write(self.text_input.text)
-		self.dismiss_popup()
-	
 
 class GuitarApp(App):
 	loadfile = ObjectProperty(None)
