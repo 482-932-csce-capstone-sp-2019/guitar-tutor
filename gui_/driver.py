e5e118505624a137a4e8de58cf5da26b8f72d910
Jerry Mares <jerrymares22696@gmail.com>
2019-05-02 16:23:22 -0500

    Cleaned up Code

--- gui_/driver.py
+++
@@ -1,8 +0,0 @@
-import os
-import htmlPy
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-app = htmlPy.AppGUI(title=u"Sample application", maximized=True, plugins=True)
-app.static_path = "."
-app.template_path = "./template"
-app.developer_mode = True
